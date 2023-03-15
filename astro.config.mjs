7549aeeb109558db2d80d4c39f52db7416720e5f
Hidetaka Okamoto <kokkoku214@gmail.com>
2023-03-15 11:40:29 +0900

    quick migrate from Next.js Tailwind UI to Astro Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import markdoc from "@astrojs/markdoc";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), markdoc({
+    nodes: {
+      heading: {
+        render: 'Heading',
+        attributes: {
+          level: {
+            type: String
+          }
+        }
+      }
+    }
+  }), tailwind(), react()]
 });
