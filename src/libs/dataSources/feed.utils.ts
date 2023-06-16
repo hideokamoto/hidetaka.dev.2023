372530eee8438b7fe3b43f932133252e3c95aea2
Hidetaka Okamoto <kokkoku214@gmail.com>
2023-06-16 14:55:31 +0900

    fix: replace rss-parser to fast-xml-parser

--- src/libs/dataSources/feed.utils.ts
+++ src/libs/dataSources/feed.utils.ts
@@ -1,10 +1,46 @@
-import Parser from 'rss-parser'
+import { XMLParser } from 'fast-xml-parser'
 
+const parser = new XMLParser()
 export const loadFeedPosts = async <T extends any>(url: string): Promise<T> => {
-    const parser = new Parser()
     try {
-      const result = await parser.parseURL(url)
-      return result as any as T
+      const response = await fetch(url)
+      const feedData = await response.text()
+      const parsedItem = parser.parse(feedData)
+      if (parsedItem.rss) {
+        const { title, link, item, lastBuildDate } = parsedItem.rss.channel
+        return {
+          lastBuildDate,
+          items: item.map((d: any) => {
+            return {
+              title: d.title,
+              content: d.description,
+              isoDate: d.pubDate,
+              link: d.link
+            }
+          }),
+          feedUrl: link,
+          title,
+          link
+        } as T
+      } else if (parsedItem.feed) {
+        const { updated, link, title, entry } = parsedItem.feed
+        return {
+          lastBuildDate: updated,
+          items: entry.map((e: any) => {
+            return {
+              title: e.title,
+              content: e.content,
+              isoDate: e.updated,
+              link: e.url
+            }
+          }),
+          feedUrl: link,
+          title,
+          link
+        } as T
+      } else {
+        throw new Error("Un supported feed type")
+      }
     } catch (e) {
       console.log('Importing feed error', e)
       throw e
--- src/libs/sanitize.ts
+++ src/libs/sanitize.ts
@@ -1,4 +1,5 @@
 export function removeHtmlTags(str: string): string {
+    if (!str) return str
     return str.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, '')
     .replace(/\ \[\&hellip\;\]/, '...');
 }
