2bab7210b12e7131a7a2add06c7296619a586d79
Hidetaka Okamoto <kokkoku214@gmail.com>
2024-02-13 12:53:46 +0900

    feat: astro v4

--- src/libs/microCMS/cloudflare.ts
+++ src/libs/microCMS/cloudflare.ts
@@ -1,12 +1,11 @@
-import { getRuntime } from "@astrojs/cloudflare/runtime";
+//import { getRuntime } from "@astrojs/cloudflare/runtime";
 import { createClient } from "microcms-js-sdk";
 import type { MicroCMSClient } from "./types";
 
 /**
  * @see https://discord.com/channels/1074874430485958716/1075435519300870244/1100981542660079646
  */
-export const createCFMicroCMSClient = (request: Request):MicroCMSClient => {
-    const runtime = getRuntime(request)
+export const createCFMicroCMSClient = (runtime: any):MicroCMSClient => {
     if (runtime && runtime.env) {
         const cfRuntimeAPIKEY = (runtime.env as any).MICROCMS_API_KEY as string
         if (cfRuntimeAPIKEY) {
