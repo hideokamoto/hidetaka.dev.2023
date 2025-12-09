8d4001b08237172b504929ac0d7ebd77a0061624
Hidetaka Okamoto <kokkoku214@gmail.com>
2025-12-09 13:49:54 +0900

    chore: resolve merge conflict in open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,4 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
-<<<<<<< HEAD
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
@@ -10,12 +9,3 @@ export default defineCloudflareConfig({
 	// Durable Objects queue for ISR background revalidation
 	queue: doQueue,
 });
-
-=======
-import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
-
-export default defineCloudflareConfig({
-  // R2 incremental cache is optional - only needed for ISR caching
-  // incrementalCache: r2IncrementalCache,
-})
->>>>>>> origin/main
